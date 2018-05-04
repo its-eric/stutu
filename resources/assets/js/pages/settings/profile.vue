3e85c14922e4b5adec6654b91f334543d628e65f
hlmya <hlmya92@gmail.com>
2018-05-04 11:33:04 +0200

    WIP profile page

--- resources/assets/js/pages/settings/profile.vue
+++ resources/assets/js/pages/settings/profile.vue
@@ -1,208 +1,136 @@
 <template>
-  <card :title="$t('your_info')">
-    <form @submit.prevent="update" @keydown="form.onKeydown($event)">
-      <alert-success :form="form" :message="$t('info_updated')"/>
+    <card :title="$t('your_info')">
+        <div class="container">
+            <ul class="nav nav-pills">
+                <li class="active"><a data-toggle="pill" href="#profile">Profile</a></li>
+                <li><a data-toggle="pill" href="#editProfile">Edit Profile</a></li>
+            </ul>
 
-      <!-- Name -->
-      <div class="form-group row">
-        <label class="col-md-3 col-form-label text-md-right">{{ $t('name') }}</label>
-        <div class="col-md-7">
-          <input v-model="form.name" :class="{ 'is-invalid': form.errors.has('name') }" class="form-control" type="text" name="name">
-          <has-error :form="form" field="name"/>
-        </div>
-      </div>
-
-      <!-- Email -->
-      <div class="form-group row">
-        <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
-        <div class="col-md-7">
-          <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
-          <has-error :form="form" field="email" />
-        </div>
-      </div>
-
-      <!-- Submit Button -->
-      <div class="form-group row">
-        <div class="col-md-9 ml-md-auto">
-          <v-button :loading="form.busy" type="success">{{ $t('update') }}</v-button>
-        </div>
-      </div>
-    </form>
-        <section id="main-content">
-      <section class="wrapper">
-        <div class="row">
-          <!-- profile-widget -->
-          <div class="col-lg-12">
-            <div class="profile-widget profile-widget-info">
-              <div class="panel-body">
-                <div class="col-lg-2 col-sm-2">
-                  <h4>Bazouzou</h4>
-                  <div class="follow-ava">
-                    <img src="img/butters.gif" alt="">
-                  </div>
-                  <h6>Tutor</h6>
-                </div>
-                <div class="col-lg-4 col-sm-4 follow-info">
-                  <p>Hello I’m Bazouzou, a leading expert in interactive and creative design.</p>
-                  <p>@Bazouzou</p>
-                  <p><i class="fa fa-twitter">Bazouzou</i></p>
-                  <h6>
-                                    <span><i class="icon_clock_alt"></i>11:00 PM</span>
-                                    <span><i class="icon_calendar"></i>16.04.18</span>
-                                    <span><i class="icon_pin_alt"></i>HU</span>
-                                </h6>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- page start-->
-        <div class="row">
-          <div class="col-lg-12">
-            <section class="panel">
-              <header class="panel-heading tab-bg-info">
-                <ul class="nav nav-tabs">
-                  <li class="active">
-                    <a data-toggle="tab" href="#profile">
-                      <i class="glyphicon glyphicon-user"></i>
-                      Profile
-                  </a>
-                  </li>
-                    <li class="">
-                    <a data-toggle="tab" href="#edit-profile">
-                      <i class="glyphicon glyphicon-edit"></i>
-                      Edit Profile
-                    </a>
-                  </li>
-                </ul>
-              </header>
-              <div class="panel-body">
-                <div class="tab-content">
-
-                  <!-- profile -->
-                  <div id="profile" class="tab-pane active">
-                    <section class="panel">
-                      <div class="bio-graph-heading">
-                        Hello I’m Bazouzou, a leading expert in interactive and creative design specializing in the mobile medium. My graduation from ELTE University with a Master of Design majoring in visual communication.
-                      </div>
-                      <div class="panel-body bio-graph-info">
-                        <h1>Bio Graph</h1>
-                        <div class="row">
-                          <div class="bio-row">
-                            <p><span>First Name: </span>Bazouzou </p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Last Name: </span>Kranjcec</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Birthday: </span>1 January 1990</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Country: </span>Hungary</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Occupation: </span>UI Designer</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Email: </span>bazouzou@mailname.com</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Mobile: </span>(+6283) 456 789</p>
-                          </div>
-                          <div class="bio-row">
-                            <p><span>Phone: </span>(+021) 956 789123</p>
-                          </div>
+            <div class="tab-content">
+                <div id="profile" class="tab-pane in active">
+                    <div class="panel-body">
+                        <div class="col-lg-12 col-sm-12">
+                            <h4>{{ user.name }}</h4>
+                            <div class="follow-ava">
+                                <img src="img/butters.gif" alt="">
+                            </div>
+                            <h6>{{ user.role }}</h6>
                         </div>
-                      </div>
-                    </section>
-                    <section>
-                      <div class="row">
-                      </div>
-                    </section>
-                  </div>
-                  <!-- edit-profile -->
-                  <div id="edit-profile" class="tab-pane">
-                    <section class="panel">
-                      <div class="panel-body bio-graph-info">
-                        <h1> Profile Info</h1>
-                        <form class="form-horizontal" role="form">
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">First Name</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="f-name" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Last Name</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="l-name" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">About Me</label>
-                            <div class="col-lg-10">
-                              <textarea name="" id="" class="form-control" cols="30" rows="5"></textarea>
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Country</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="c-name" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Birthday</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="b-day" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Occupation</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="occupation" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Email</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="email" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Mobile</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="mobile" placeholder=" ">
-                            </div>
-                          </div>
-                          <div class="form-group">
-                            <label class="col-lg-2 control-label">Website URL</label>
-                            <div class="col-lg-6">
-                              <input type="text" class="form-control" id="url" placeholder="http://www.demowebsite.com ">
-                            </div>
-                          </div>
-
-                          <div class="form-group">
-                            <div class="col-lg-offset-2 col-lg-10">
-                              <button type="submit" class="btn btn-primary">Save</button>
-                              <button type="button" class="btn btn-danger">Cancel</button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </section>
-                  </div>
+                        <div class="col-lg-12 col-sm-12 follow-info">
+                            <p>Hello I’m {{ user.name }}, a leading expert in interactive and creative design.</p>
+                            <p>{{ user.name }}</p>
+                            <p><i class="fa fa-twitter">{{ user.name }}</i></p>
+                            <h6>
+                                <span><i class="icon_clock_alt"></i>11:00 PM</span>
+                                <span><i class="icon_calendar"></i>16.04.18</span>
+                                <span><i class="icon_pin_alt"></i>HU</span>
+                            </h6>
+                        </div>
+                        <!-- profile -->
+                        <div id="profile" class="tab-pane active">
+                            <section class="panel">
+                                <div class="bio-graph-heading">
+                                Hello I’m Bazouzou, a leading expert in interactive and creative design specializing in the mobile medium. My graduation from ELTE University with a Master of Design majoring in visual communication.
+                                </div>
+                                <div class="panel-body bio-graph-info">
+                                    <h1>Bio Graph</h1>
+                                    <div class="row">
+                                        <div class="bio-row">
+                                            <p><span>First Name: </span>Bazouzou </p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Last Name: </span>Kranjcec</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Birthday: </span>1 January 1990</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Country: </span>Hungary</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Occupation: </span>UI Designer</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Email: </span>bazouzou@mailname.com</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Mobile: </span>(+6283) 456 789</p>
+                                        </div>
+                                        <div class="bio-row">
+                                            <p><span>Phone: </span>(+021) 956 789123</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="row">
+                                </div>
+                            </section>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </section>
-          </div>
+                <div id="editProfile" class="tab-pane fade">
+                    <div class="panel-body">
+                        <form @submit.prevent="update" @keydown="form.onKeydown($event)">
+                            <alert-success :form="form" :message="$t('info_updated')"/>
+
+                            <!-- Name -->
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label text-md-right">{{ $t('name') }}</label>
+                                <div class="col-md-7">
+                                    <input v-model="form.name" :class="{ 'is-invalid': form.errors.has('name') }" class="form-control" type="text" name="name">
+                                    <has-error :form="form" field="name"/>
+                                </div>
+                            </div>
+
+                            <!-- Email -->
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
+                                <div class="col-md-7">
+                                    <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+                                    <has-error :form="form" field="email" />
+                                </div>
+                            </div>
+
+                            <!-- Experience -->
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label text-md-right">{{ $t('experience') }}</label>
+                                <div class="col-md-7">
+                                    <input v-model="form.experience" :class="{ 'is-invalid': form.errors.has('experience') }" class="form-control" type="experience" name="experience">
+                                    <has-error :form="form" field="experience" />
+                                </div>
+                            </div>
+
+                            <!-- url -->
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label text-md-right">{{ $t('url') }}</label>
+                                <div class="col-md-7">
+                                    <input v-model="form.url" :class="{ 'is-invalid': form.errors.has('url') }" class="form-control" type="url" name="url">
+                                    <has-error :form="form" field="url" />
+                                </div>
+                            </div>
+
+                            <!-- expectation -->
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label text-md-right">{{ $t('expectation') }}</label>
+                                <div class="col-md-7">
+                                    <input v-model="form.expectation" :class="{ 'is-invalid': form.errors.has('expectation') }" class="form-control" type="expectation" name="expectation">
+                                    <has-error :form="form" field="expectation" />
+                                </div>
+                            </div>
+
+                            <!-- Submit Button -->
+                            <div class="form-group row">
+                                <div class="col-md-9 ml-md-auto">
+                                    <v-button :loading="form.busy" type="success">{{ $t('update') }}</v-button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- page end-->
-      </section>
-    </section>
-
-  </card>
+    </card>
 </template>
 
 <script>
@@ -219,7 +147,10 @@ export default {
   data: () => ({
     form: new Form({
       name: '',
-      email: ''
+      email: '',
+      experience: '',
+      url: '',
+      expectation: ''
     })
   }),
 
